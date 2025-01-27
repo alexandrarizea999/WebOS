3300a6cc9244169167a450cc4918f8479f2f6372
alexandrarizea999 <alexandrariEa222@yahoo.com>
2025-01-28 00:47:12 +0200

    homepag styling components click button

---
+++ React-tutorial/react-blog-tutorial/src/Navbar.jsx
@@ -0,0 +1,12 @@
+const Navbar = () => {
+    return ( 
+        <nav className="navbar">
+            <h1>WebMed</h1>
+            <div className="links">
+                <a href="/">Home</a>
+                <a href="/create-appointment">Create Appointment</a>
+            </div>
+        </nav>
+    );
+}
+export default Navbar;
